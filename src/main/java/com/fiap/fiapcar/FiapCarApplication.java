404a971389de1ed3e99b110ee54c5d6c75fc9c9c
Dilan Lima <dilanlima@Dilans-MacBook-Pro.local>
2025-08-07 19:00:56 -0300

    feature: initial commit

---
+++ src/main/java/com/fiap/fiapcar/FiapCarApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.fiapcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapCarApplication.class, args);
+	}
+
+}
